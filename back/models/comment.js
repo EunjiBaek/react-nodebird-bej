421382145c36798a65d3fe5e1c9877772e2a308e
ejbaek <ejbaek@ejbaekui-MacBookAir.local>
2021-11-28 18:26:47 +0900

    package change

--- back/models/comment.js
+++ back/models/comment.js
@@ -9,13 +9,11 @@ module.exports = class Comment extends Model {
                 type: DataTypes.TEXT,
                 allowNullL: false,
             },
-            // UserId: {}
-            // PostId: {}
         }, {
             modelName: 'Comment',
             tableName: 'comments',
             charset: 'utf8mb4',
-            collate: 'utf8mb4_general_ci', // 이모티콘 저장
+            collate: 'utf8mb4_general_ci',
             sequelize,
         });
     }
